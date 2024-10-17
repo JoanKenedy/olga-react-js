fd4c605b2198b9896254edd2c1e10f58da8c3f02
JoanKenedy <joankenedi9311@gmail.com>
2024-10-17 13:19:59 -0600

    framr--motion

--- src/components/SobreMi/Experiencia.jsx
+++ src/components/SobreMi/Experiencia.jsx
@@ -4,7 +4,12 @@ import Doctora from "../../assets/doctor.png";
 import Hospital from "../../assets/hospital.png";
 import FondoVideo from "../../assets/video-block-img.jpg";
 import BtnVideo from "../../assets/boton-de-play.png";
+import CountUp from "react-countup";
+import { useState } from "react";
+import ScrollTrigger from "react-scroll-trigger";
+
 export const Experiencia = () => {
+  const [conuterOn, setCounterUp] = useState(false);
   return (
     <>
       <section className="bg-slate-100 w-full">
@@ -14,7 +19,18 @@ export const Experiencia = () => {
               <div className="bg-zinc-200 w-[70px] h-[70px] rounded-full flex justify-center items-center md:w-[90px] md:h-[90px]">
                 <img src={Bag} alt="" className="w-[40px] md:w-[50px] " />
               </div>
-              <h2 className="font-bold text-2xl">10</h2>
+
+              <ScrollTrigger
+                onEnter={() => setCounterUp(true)}
+                onExit={() => setCounterUp(false)}
+              >
+                {conuterOn && (
+                  <h2 className="font-bold text-2xl">
+                    <CountUp start={0} end={10} duration={4} delay={0} />
+                  </h2>
+                )}
+              </ScrollTrigger>
+
               <p className="text-center font-medium text-gray-500 text-lg">
                 Años de experiencia
               </p>
@@ -23,7 +39,17 @@ export const Experiencia = () => {
               <div className="bg-zinc-200 w-[70px] h-[70px] rounded-full flex justify-center items-center md:w-[90px] md:h-[90px]">
                 <img src={Feliz} alt="" className="w-[40px] md:w-[50px] " />
               </div>
-              <h2 className="font-bold text-2xl">2754</h2>
+              <ScrollTrigger
+                onEnter={() => setCounterUp(true)}
+                onExit={() => setCounterUp(false)}
+              >
+                {conuterOn && (
+                  <h2 className="font-bold text-2xl">
+                    <CountUp start={0} end={3750} duration={4} delay={0} />
+                  </h2>
+                )}
+              </ScrollTrigger>
+
               <p className="text-center font-medium text-gray-500 text-lg">
                 Pacientes felices
               </p>
@@ -32,16 +58,36 @@ export const Experiencia = () => {
               <div className="bg-zinc-200 w-[70px] h-[70px] rounded-full flex justify-center items-center md:w-[90px] md:h-[90px]">
                 <img src={Doctora} alt="" className="w-[40px] md:w-[50px] " />
               </div>
-              <h2 className="font-bold text-2xl">4.8</h2>
+              <ScrollTrigger
+                onEnter={() => setCounterUp(true)}
+                onExit={() => setCounterUp(false)}
+              >
+                {conuterOn && (
+                  <h2 className="font-bold text-2xl">
+                    <CountUp start={0} end={4257} duration={4} delay={0} />
+                  </h2>
+                )}
+              </ScrollTrigger>
+
               <p className="text-center font-medium text-gray-500 text-lg">
-                Calificación de doctora
+                Servicios con éxito
               </p>
             </li>
             <li className="flex items-center justify-center flex-col border border-gray-300 rounded-lg py-[40px] px-[20px] gap-2 bg-white shadow-lg">
               <div className="bg-zinc-200 w-[70px] h-[70px] rounded-full flex justify-center items-center md:w-[90px] md:h-[90px]">
                 <img src={Hospital} alt="" className="w-[40px] md:w-[50px] " />
               </div>
-              <h2 className="font-bold text-2xl">1</h2>
+              <ScrollTrigger
+                onEnter={() => setCounterUp(true)}
+                onExit={() => setCounterUp(false)}
+              >
+                {conuterOn && (
+                  <h2 className="font-bold text-2xl">
+                    <CountUp start={10} end={1} duration={4} delay={0} />
+                  </h2>
+                )}
+              </ScrollTrigger>
+
               <p className="text-center font-medium text-gray-500 text-lg">
                 Hospital de Primer nivel
               </p>
--- src/components/SobreMi/LineTime.jsx
+++ src/components/SobreMi/LineTime.jsx
@@ -1,5 +1,20 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 export const LineTime = () => {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+  const boxVariant = {
+    visible: { opacity: 1, transition: { duration: 1.3 } },
+    hidden: { opacity: 0 },
+  };
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
   return (
     <>
       <div className="line-time w-full pt-8 ">
@@ -9,9 +24,10 @@ export const LineTime = () => {
           </h2>
           <div className="linea  bg-gray-400 "></div>
           <motion.article
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 1.5 } }}
-            viewport={{ once: true }}
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
           >
             <article className="item-history h-[75px] w-[85%] flex justify-center items-center color-text   ml-6 rounded-lg   text-xs text-center px-2 md:absolute md:top-10 md:left-0 md:w-[45%] md:p-10 md:text-base ">
               1.- Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -19,9 +35,10 @@ export const LineTime = () => {
             </article>
           </motion.article>
           <motion.article
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 2 } }}
-            viewport={{ once: true }}
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
           >
             <article className="item-history h-[75px] w-[85%] flex justify-center items-center color-text  ml-6 rounded-lg   text-xs text-center px-2 md:absolute md:right-5 md:top-16 md:w-[45%] md:p-10  md:text-base ">
               2.- Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -30,9 +47,10 @@ export const LineTime = () => {
           </motion.article>
 
           <motion.article
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 2.5 } }}
-            viewport={{ once: true }}
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
           >
             <article className="item-history h-[75px] w-[85%] flex justify-center items-center color-text  ml-6 rounded-lg   text-xs text-center px-2 md:absolute md:left-0 md:top-40 md:w-[45%] md:p-10 md:text-base ">
               3.- Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -41,9 +59,10 @@ export const LineTime = () => {
           </motion.article>
 
           <motion.article
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 3 } }}
-            viewport={{ once: true }}
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
           >
             <article className="item-history h-[75px] w-[85%] flex justify-center items-center color-text ml-6 rounded-lg   text-xs text-center px-2 md:absolute md:right-5 md:top-48 md:w-[45%] md:p-10 md:text-base  ">
               4.- Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -52,9 +71,10 @@ export const LineTime = () => {
           </motion.article>
 
           <motion.article
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 3.5 } }}
-            viewport={{ once: true }}
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
           >
             <article className="item-history h-[75px] w-[85%] flex justify-center items-center color-text  ml-6 rounded-lg   text-xs text-center px-2 md:absolute md:left-0 md:top-72 md:w-[45%] md:p-10 md:text-base ">
               5.- Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -62,9 +82,10 @@ export const LineTime = () => {
             </article>
           </motion.article>
           <motion.article
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1, transition: { duration: 4 } }}
-            viewport={{ once: true }}
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}
           >
             <article className="item-history h-[75px] w-[85%] flex justify-center items-center color-text  ml-6 rounded-lg  text-xs text-center px-2 md:absolute md:right-5 md:top-[320px] md:w-[45%] md:p-10 md:text-base  ">
               6.- Lorem, ipsum dolor sit amet consectetur adipisicing elit.
